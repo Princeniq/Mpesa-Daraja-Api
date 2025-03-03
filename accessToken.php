9e10876061e9ba8a46b6b36a75cb030c5491d7b3
Nic Kurgat <kurgatnk@gmail.com>
2025-03-03 16:59:34 +0300

    Sending our access token file

---
+++ accessToken.php
@@ -0,0 +1,16 @@
+<?php
+$consumerKey = "PGIUlgRo5Iv5BqodsYcG6NEAzGsxitXeQDzHdOTAw5ABfk49"; 
+$consumerSecret = "Auc4QzF2C9XGcVLwkmzlGOqMrta2NWBmNUstiBxJOY6bLzCuXZWE9AUpwLEPRAsh"; 
+//ACCESS TOKEN URL
+$access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+$headers = ['Content-Type:application/json; charset=utf8'];
+$curl = curl_init($access_token_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($curl, CURLOPT_HEADER, FALSE);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+echo $access_token = $result->access_token;
+curl_close($curl);
